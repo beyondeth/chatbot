1a588f31ab8a767f0be035f783dc5a2fcb9da4de
park992 <luticek@naver.com>
2025-05-21 19:59:35 +0900

    d

--- selenium_youtube_desc.py
+++ selenium_youtube_desc.py
@@ -8,6 +8,7 @@ import time
 url = sys.argv[1]
 
 options = Options()
+options.binary_location = "/usr/bin/chromium-browser"
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
